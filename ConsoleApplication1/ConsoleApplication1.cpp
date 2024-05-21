f6bc9f0057d0f986084e96d727a5f356bc22cec6
Student <Student@TA4-03>
2024-05-21 18:20:46 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+#define FirstVal 3
+#define SecondVal 3
+#define myval 5
+#if FirstVal*SecondVal == 9
+int main() {
+	cout << "9\n" << myval;
+	return 0;
+}
+#elif FirstVal*SecondVal == 15
+int main() {
+	cout << "15\n";
+	return 0;
+}
+#else
+int main() {
+	cout << "Not 9 or 15\n";
+	return 0;
+#endif
